5e1e0617c2f92229b25d9371365825de6ddee3e1
tiper2066 <tiper2066@gmail.com>
2025-07-28 09:50:59 +0900

    complete 3d iPhone animation site

--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -1,99 +1,87 @@
 'use client';
 
-import { useGSAP } from '@gsap/react';
-import gsap from 'gsap';
-import { useRef, useState } from 'react'; // ********************* useRef, useState Hook
-import ModelView from './ModelView'; // ********************* 실제 개별 모델 컴포넌트
-import { yellowImg } from '@/utils'; // ********************* 모델 이미지(노랑)
-import * as THREE from 'three'; // ********************* three.js
-import { Canvas } from '@react-three/fiber'; // ***************** 3d canvas
-import { View } from '@react-three/drei'; // ***************** 3d view
-import { models, sizes } from '@/constants'; // ********************* 모델 및 Size 정보 데이터 가져오기
+import { useRef, useState } from 'react'; // ********************* useEffect 추가
+import ModelView from './ModelView';
+import { yellowImg } from '@/utils'; //  모델 이미지(노랑)
+import * as THREE from 'three'; //  three.js
+import { Canvas } from '@react-three/fiber'; //  3d canvas
+import { View } from '@react-three/drei'; //  3d view
+import { models, sizes } from '@/constants'; //  모델 및 Size 정보 데이터 가져오기
 
 const Model = () => {
-    const [size, setSize] = useState('small'); // ********************** 사이즈 설정 상태변수
-    // *********************************************** 모델 정보 상태 변수 (초기 정보값 설정)
+    const [size, setSize] = useState('small'); //  사이즈 설정 상태변수
     const [model, setModel] = useState({
         title: 'iPhone 15 Pro in Natural Titanium', // 모델명
         color: ['#8F8A81', '#FFE7B9', '#6F6C64'], // 모델 색상 배열
         img: yellowImg,
     });
 
-    // *********************************************** 모델 뷰를 위한 카메라 컨트롤 참조 변수(Ref)
+    //  모델 뷰를 위한 카메라 컨트롤 참조 변수(Ref)
     const cameraControlSmallRef = useRef(null);
     const cameraControlLargeRef = useRef(null);
 
-    // *********************************************** 실제 모델 참조 변수
-    const small = useRef(new THREE.Group()); // 작은 모델 참조
-    const large = useRef(new THREE.Group()); // 큰 모델 참조
+    //  실제 모델 참조 변수
+    const small = useRef(new THREE.Group());
+    const large = useRef(new THREE.Group());
 
-    // *********************************************** 모델 회전각 상태 변수
+    //  모델 회전각 상태 변수
     const [smallRotation, setSmallRotation] = useState(0); // 작은 모델 회전각
     const [largeRotation, setLargeRotation] = useState(0); //  큰 모델 회전각
 
-    useGSAP(() => {
-        gsap.to('#heading', {
-            opacity: 1, // 보이기
-            y: 0, // 원래위치로 이동
-        });
-    }, []);
+    const canvasWrapperRef = useRef<HTMLDivElement | null>(null); //  3d 모델을 표시할 캔바스 참조
 
     return (
         <section className='common-padding'>
             <div className='screen-max-width'>
-                {/* ----- 섹션 제목: 회색글자, 동적 크기 조정, 숨겨짐, 80px 아래 배치 ----- */}
                 <h1 id='heading' className='section-heading'>
                     Take a closer look.
                 </h1>
-                {/* ----- 모델 컨테이너 ----- */}
+
                 <div className='flex flex-col items-center mt-5'>
-                    <div className='w-full h-[75vh] md:h-[90vh] overflow-hidden relative'>
-                        {/* *********************** 실제 모델 이미지 (small) --------- */}
+                    {/* canvasWrapperRef를 div에 연결 */}
+                    <div
+                        className='w-full h-[75vh] md:h-[90vh] overflow-hidden relative'
+                        ref={canvasWrapperRef}
+                    >
                         <ModelView
-                            index={1} // 랜더링될 모델 번호
-                            groupRef={small} // 동일한 그룹 참조
-                            gsapType='view1' // gsap 애니메이션 타입
-                            controlRef={cameraControlSmallRef} // 카메라 제어를 위한 참조
-                            setRotationState={setSmallRotation} // 회전각 상태변수
-                            item={model} // 모델 정보
-                            size={size} // 사이즈 정보
+                            index={1}
+                            groupRef={small}
+                            gsapType='view1'
+                            controlRef={cameraControlSmallRef}
+                            setRotationState={setSmallRotation}
+                            item={model}
+                            size={size}
                         />
-                        {/* *********************** 실제 모델 이미지 (large) --------- */}
                         <ModelView
-                            index={2} // 랜더링될 모델 번호
-                            groupRef={large} // 동일한 그룹 참조
-                            gsapType='view2' // gsap 애니메이션 타입
-                            controlRef={cameraControlLargeRef} // 카메라 제어를 위한 참조
-                            setRotationState={setLargeRotation} // 회전각 상태변수
-                            item={model} // 모델 정보
-                            size={size} // 사이즈 정보
+                            index={2}
+                            groupRef={large}
+                            gsapType='view2'
+                            controlRef={cameraControlLargeRef}
+                            setRotationState={setLargeRotation}
+                            item={model}
+                            size={size}
                         />
-                        {/* *********************** 모델이 배치될 캔버스 --------- */}
                         <Canvas
-                            className='w-full h-full' // 부모 컨테이너 영역 차지
+                            className='w-full h-full'
                             style={{
-                                position: 'fixed', // 부모 컨테이너에 고정
+                                position: 'fixed',
                                 top: 0,
                                 bottom: 0,
                                 left: 0,
                                 right: 0,
                                 overflow: 'hidden',
                             }}
-                            eventSource={document.getElementById('root')} // app > page.tsx 의 main id='root' 참조
+                            eventSource={canvasWrapperRef} // ref 객체 자체를 전달
                         >
                             <View.Port />
                         </Canvas>
                     </div>
 
-                    {/* *********************** 모델 정보 텍스트 영역 --------- */}
                     <div className='mx-auto w-full'>
-                        {/* ----- 작은 타이틀  ----- */}
                         <p className='text-sm font-light text-center mb-5'>
                             {model.title}
                         </p>
-                        {/* ----- 추가 정보 영역   ----- */}
                         <div className='flex-center'>
-                            {/* ----- 색상 선택 콘트롤 영역   ----- */}
                             <ul className='color-container'>
                                 {models.map((item, i) => (
                                     <li
@@ -106,7 +94,6 @@ const Model = () => {
                                     />
                                 ))}
                             </ul>
-                            {/* ----- 우측 Size 선택 스위치 버튼 영역   ----- */}
                             <button className='size-btn-container'>
                                 {sizes.map(({ label, value }) => (
                                     <span
@@ -135,4 +122,5 @@ const Model = () => {
         </section>
     );
 };
+
 export default Model;
--- src/components/ModelView.tsx
+++ src/components/ModelView.tsx
@@ -1,7 +1,24 @@
-import { PerspectiveCamera, View } from '@react-three/drei';
+import { OrbitControls, PerspectiveCamera, View } from '@react-three/drei'; //  OrbitControls 추가
 import { Suspense } from 'react';
 import Lights from './Lights';
-import IPhone from './iPhone'; // ******************* iPhone 모델 컴포넌트
+import IPhone from './iPhone'; //  iPhone 모델 컴포넌트
+import * as THREE from 'three'; //  three.js
+import { OrbitControls as OrbitControlsImpl } from 'three-stdlib'; //  OrbitControls 구현
+import Loader from './Loader'; // ******************************** 사용자 정의 로더 추가
+
+interface ModelViewProps {
+    index: number;
+    groupRef: React.MutableRefObject<THREE.Group | null>;
+    gsapType: string;
+    controlRef: React.MutableRefObject<OrbitControlsImpl | null>;
+    setRotationState: (angle: number) => void;
+    size: string;
+    item: {
+        title: string;
+        color: string[];
+        img: string;
+    };
+}
 
 const ModelView = ({
     index,
@@ -11,7 +28,7 @@ const ModelView = ({
     setRotationState,
     size,
     item,
-}) => {
+}: ModelViewProps) => {
     return (
         <View
             index={index}
@@ -28,10 +45,39 @@ const ModelView = ({
             {/* Ambient Light : 환경 조명 */}
             <Lights />
 
-            <Suspense fallback={<div>Loading</div>}>
-                <IPhone />
-            </Suspense>
+            {/*  OrbitControls 추가  */}
+            <OrbitControls
+                makeDefault
+                ref={controlRef}
+                enableZoom={false} // 줌 방지
+                enablePan={false} // 이동 방지
+                rotateSpeed={0.4}
+                target={new THREE.Vector3(0, 0, 0)} // 모델을 중앙에 배치
+                onEnd={() => {
+                    if (controlRef.current) {
+                        // 이동을 멈추면 모델의 회전상태를 업데이트
+                        setRotationState(
+                            controlRef.current.getAzimuthalAngle()
+                        );
+                    }
+                }}
+            />
+            {/*  모델을 group 으로 갑싼 후 크기, 위치 제어 속성 추가  */}
+            <group
+                ref={groupRef}
+                name={index === 1 ? 'small' : 'large'}
+                position={[0, 0, 0]}
+            >
+                <Suspense fallback={<Loader />}>
+                    <IPhone
+                        scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+                        item={item}
+                        size={size}
+                    />
+                </Suspense>
+            </group>
         </View>
     );
 };
+
 export default ModelView;
